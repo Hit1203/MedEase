e3b468d7bda561b7d8b0a7b0602829f1b295c950
Niraj kc <niraj.kc.128@gmail.com>
2023-09-24 14:26:23 +0530

    patient upcoming appointment ui updated

--- lib/Home_Screen Pages/Custom_Drawer/upcoming_appointments.dart
+++ lib/Home_Screen Pages/Custom_Drawer/upcoming_appointments.dart
@@ -24,40 +24,56 @@ class _UpcomingAppointmentsState extends State<UpcomingAppointments> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.black,
         title: Text("Upcoming Appointments"),
       ),
 
-      body: Column(
-        children: [
-          FutureBuilder(
-              future: AppointmentRequests.isThereAppointment(curUser.userID!),
-              builder: (context, snapshot){
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            FutureBuilder(
+                future: AppointmentRequests.isThereAppointment(curUser.userID!),
+                builder: (context, snapshot){
 
-                if(snapshot.connectionState == ConnectionState.done){
-                  if(snapshot.hasError){
-                    return Container(child: Text(""),);
-                  }
-                  else if (snapshot.hasData){
-                    //todo
-                    // print("is Appo res: ${snapshot.data}  ${curUser.auth_token}");
-                    final res = snapshot.data["responseData"]["appointments"];
-                    if(res.length == 0){
-                      return Container();
+                  if(snapshot.connectionState == ConnectionState.done){
+                    if(snapshot.hasError){
+                      return Container(child: Text("Error"),);
                     }
-                    else{
-                      return Column(
-                        children: res.map((e) => appointmentCard(context, MyAppointment.fromJSON(e), callback)).toList(),
-                      );
-                    }
+                    else if (snapshot.hasData){
+                      //todo
+                      // print("is Appo res: ${snapshot.data}  ${curUser.auth_token}");
+                      final res = snapshot.data["responseData"]["appointments"];
+                      // print("res: ${res[0].runtimeType}" );
+                      List<Widget> wl = [];
+                      List<dynamic> ml = res.map((e) {
+                        var ac = appointmentCard(context, MyAppointment.fromJSON(e),
+                            callback);
+                        wl.add(ac);
+                        return ac;
+                      }).toList();
+                      print("ml:${ml.runtimeType}");
 
+                      if(res.length == 0){
+                        return Container(
+                          height: MediaQuery.of(context).size.height,
+                          width: MediaQuery.of(context).size.width,
+                          child: Text("No Upcoming Appointments", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),),);
+                      }
+                      else{
+                        return Column(
+                          children: wl,
+                        );
+                      }
+
+                    }
                   }
+
+                  return Center(child: CircularProgressIndicator(),);
                 }
+            ),
 
-                return Container();
-              }
-          ),
-
-        ],
+          ],
+        ),
       ),
     );
   }
