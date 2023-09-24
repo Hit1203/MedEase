7153e9ccbdae778240a5fc736740e470ea89c14a
Harsh Rajput <harshmori0521@gmail.com>
2023-09-24 11:35:16 +0530

    sddsf:

--- lib/Home_Screen Pages/Settings/Doctor/doctor_settings.dart
+++ lib/Home_Screen Pages/Settings/Doctor/doctor_settings.dart
@@ -17,6 +17,15 @@ class _DoctortSettingsState extends State<DoctorSettings> {
   final  ageControl = TextEditingController();
   int? Age = curUser.age ;
 
+  bool isMonday = false ;
+  bool isTuesday = false ;
+  bool isWednesday = false ;
+  bool isThusday = false ;
+  bool isFriday = false ;
+  bool isSaturday = false ;
+  bool isSunday = true ;
+  List<String>? nonWorkingDays = ["Sunday"];
+
   final weightControl = TextEditingController();
   int? Weigth = curUser.age;
 
@@ -50,139 +59,110 @@ class _DoctortSettingsState extends State<DoctorSettings> {
                 icon: Icon(Icons.password),
                 padding: 0.16,
                 ontap: () {
-                  showDialog(context: context, builder: (context)
-                  {
-                    return AlertDialog(
-                      content: Container(
-                        width: MediaQuery.of(context).size.width*0.7,
-                        child: TextFormField(
-                          controller: passControl,
-                          decoration: InputDecoration(
-                            labelText: "Enter your Updated Password : ",
-                            border: OutlineInputBorder(),
 
-                          ),
-                          keyboardType: TextInputType.number,
-                          maxLines: null,
-                          // onChanged: ,
-                        ),
-
-                      ),
-                      title: Text("Set Password"),
-                      actions: [
-                        TextButton(
-                            onPressed: () {
-                              setState(() {
-
-                              });
-                            },
-                            child: Text("Save")),
-                      ],
-                    );
-                  }) ;
                 },
               ),
               SizedBox(height: 10,),
-              Container(
-                child: Row(
-                  children: [
-                    Row(
-                      children: [
-                        Padding(
-                          padding:  EdgeInsets.only(top: 5.0,left: 12),
-                          child: Container(
-                            width: 40,
-                            height: 40,
-                            decoration: BoxDecoration(
-                              border: Border.all(),
-                              shape: BoxShape.circle,
-                            ),
-                            child:Icon(Icons.holiday_village_sharp),
-
-                          ),
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        SizedBox(height: 15,),
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(top: 2.0,left: 12),
-                              child: Text("Non Working Days:",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(top: 2.0,left: 12),
-                              child: Text("${curUser.nonWorkingDays}",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        children: [
-                          Padding(
-                              padding:  EdgeInsets.only(top: 5.0,left:MediaQuery.of(context).size.width*0.14),
-                              child: InkWell(
-                                  onTap: (){
-                                    showDialog(context: context, builder: (context)
-                                    {
-                                      return AlertDialog(
-                                        content: Container(
-                                          width: MediaQuery.of(context).size.width*0.7,
-                                          child: TextFormField(
-                                            controller: heigthControl,
-                                            decoration: InputDecoration(
-                                              labelText: "Update Non Working Days : ",
-                                              border: OutlineInputBorder(),
-
-                                            ),
-                                            keyboardType: TextInputType.number,
-                                            maxLines: null,
-                                            // onChanged: ,
-                                          ),
-
-                                        ),
-                                        title: Text("Update Working Days"),
-                                        actions: [
-                                          TextButton(
-                                              onPressed: () {
-                                                setState(() {
-
-                                                });
-                                              },
-                                              child: Text("Save")),
-                                        ],
-                                      );
-                                    }) ;
-                                  },
-                                  child: Icon(Icons.edit)
-                              )
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                decoration: BoxDecoration (
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(),
-                  color: Colors.transparent,
-                ),
-                width: MediaQuery.of(context).size.width*0.85,
-                height: MediaQuery.of(context).size.height*0.11,
-              ),
+              // Container(
+              //   child: Row(
+              //     children: [
+              //       Row(
+              //         children: [
+              //           Padding(
+              //             padding:  EdgeInsets.only(top: 5.0,left: 12),
+              //             child: Container(
+              //               width: 40,
+              //               height: 40,
+              //               decoration: BoxDecoration(
+              //                 border: Border.all(),
+              //                 shape: BoxShape.circle,
+              //               ),
+              //               child:Icon(Icons.holiday_village_sharp),
+              //
+              //             ),
+              //           )
+              //         ],
+              //       ),
+              //       Column(
+              //         children: [
+              //           SizedBox(height: 15,),
+              //           Row(
+              //             children: [
+              //               Padding(
+              //                 padding: const EdgeInsets.only(top: 2.0,left: 12),
+              //                 child: Text("Non Working Days:",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+              //               ),
+              //             ],
+              //           ),
+              //           Row(
+              //             children: [
+              //               Padding(
+              //                 padding: const EdgeInsets.only(top: 2.0,left: 12),
+              //                 child: Text("${curUser.nonWorkingDays}",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+              //               ),
+              //             ],
+              //           ),
+              //         ],
+              //       ),
+              //       SingleChildScrollView(
+              //         scrollDirection: Axis.horizontal,
+              //         child: Row(
+              //           children: [
+              //             Padding(
+              //                 padding:  EdgeInsets.only(top: 5.0,left:MediaQuery.of(context).size.width*0.14),
+              //                 child: InkWell(
+              //                     onTap: (){
+              //                       showDialog(context: context, builder: (context)
+              //                       {
+              //                         return AlertDialog(
+              //                           content: Container(
+              //                             width: MediaQuery.of(context).size.width*0.7,
+              //                             child: TextFormField(
+              //                               controller: heigthControl,
+              //                               decoration: InputDecoration(
+              //                                 labelText: "Update Non Working Days : ",
+              //                                 border: OutlineInputBorder(),
+              //
+              //                               ),
+              //                               keyboardType: TextInputType.number,
+              //                               maxLines: null,
+              //                               // onChanged: ,
+              //                             ),
+              //
+              //                           ),
+              //                           title: Text("Update Working Days"),
+              //                           actions: [
+              //                             TextButton(
+              //                                 onPressed: () {
+              //                                   setState(() {
+              //
+              //                                   });
+              //                                 },
+              //                                 child: Text("Save")),
+              //                           ],
+              //                         );
+              //                       }) ;
+              //                     },
+              //                     child: Icon(Icons.edit)
+              //                 )
+              //             )
+              //           ],
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              //   decoration: BoxDecoration (
+              //     borderRadius: BorderRadius.circular(20),
+              //     border: Border.all(),
+              //     color: Colors.transparent,
+              //   ),
+              //   width: MediaQuery.of(context).size.width*0.85,
+              //   height: MediaQuery.of(context).size.height*0.11,
+              // ),
               CustomCon(
-                Name: "Working :  ${curUser.height}",
+                Name: "Working Hours :  ${curUser.height}",
                 icon: Icon(Icons.height),
-                padding: 0.27,
+                padding: 0.09,
                 ontap: () {
                   showDialog(context: context, builder: (context)
                   {
@@ -192,7 +172,7 @@ class _DoctortSettingsState extends State<DoctorSettings> {
                         child: TextFormField(
                           controller: heigthControl,
                           decoration: InputDecoration(
-                            labelText: "Enter Your Current Height : ",
+                            labelText: "Update Non Working Hours : ",
                             border: OutlineInputBorder(),
 
                           ),
@@ -202,43 +182,7 @@ class _DoctortSettingsState extends State<DoctorSettings> {
                         ),
 
                       ),
-                      title: Text("Current Heigth"),
-                      actions: [
-                        TextButton(
-                            onPressed: () {
-                              setState(() {
-
-                              });
-                            },
-                            child: Text("Save")),
-                      ],
-                    );
-                  }) ;
-                },),
-              CustomCon(
-                Name: "Age  : ${curUser.age}",
-                icon: Icon(Icons.timelapse),
-                padding:0.33,
-                ontap: () {
-                  showDialog(context: context, builder: (context)
-                  {
-                    return AlertDialog(
-                      content: Container(
-                        width: MediaQuery.of(context).size.width*0.7,
-                        child: TextFormField(
-                          controller: ageControl,
-                          decoration: InputDecoration(
-                            labelText: "Enter Your Current Age : ",
-                            border: OutlineInputBorder(),
-
-                          ),
-                          keyboardType: TextInputType.number,
-                          maxLines: null,
-                          // onChanged: ,
-                        ),
-
-                      ),
-                      title: Text("Current Age"),
+                      title: Text("Current Hours"),
                       actions: [
                         TextButton(
                             onPressed: () {
